5d0fb6b0fe9e7ac3031ba456304cd7d559f007fb
Yaroslav Pelyukh <y.pelyukh@gmail.com>
2024-08-15 13:02:07 +0300

    Initial commit

---
+++ MauiTipsAndTricks/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiTipsAndTricks;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MauiTipsAndTricks/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiTipsAndTricks;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
